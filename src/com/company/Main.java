c5fbb969a8121c316bda1fac23466f967fb4060b
VladimirPopkov <vovanpopkov@mail.ru>
2020-11-17 00:09:01 +0300

    Task4_2.Пункт 9 задание 2. Внесены комментарии

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,18 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         Fruit[] fruits = new Fruit[4];
-        AbstractFabric[] fabrics = new AbstractFabric[10];
-        Fruit[] fruitsFactory = new Fruit[20];
-        int fabricNumber = 1;
+        AbstractFabric[] fabrics = new AbstractFabric[10]; //Task4_2
+        Fruit[] fruitsFactory = new Fruit[20]; //Task4_2
+        int fabricNumber = 1; //Task4_2
         for (int i = 0; i < fruits.length; i++){
             fruits[i] = getRandomFruit(random.nextInt(3)+1);
             System.out.println(fruits[i].toString());
         }
+        //Task4_2
         for (int i = 0; i < fabrics.length; i++){
             fabrics[i] = getRandomFabric(random.nextInt(4)+1);
         }
+        //Task4_2
         for (int i = 0; i < fruitsFactory.length; i++){
             fruitsFactory[i] = getRandomFabric(fabricNumber).makeFruit();
             if (i%2 != 0)
@@ -39,6 +41,7 @@ public class Main {
         }
     }
 
+    //Task4_2
     static AbstractFabric getRandomFabric(int fabricNumber){
         switch (fabricNumber){
             case 1:
